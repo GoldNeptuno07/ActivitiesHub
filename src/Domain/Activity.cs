de8dfa1f157fbc72f744e2de8c56d0cea4c39ea6
Angel Cruz <angelcruz@Angels-MacBook-Air.local>
2026-09-07 10:25:09 -0600

    feat(parcial01) - add entity and EF boilerplate

---
+++ src/Domain/Activity.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain;
+
+public class Activity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Title { set; get; }
+    public DateTime Date { set; get; } = DateTime.MinValue;
+    public required string Description { set; get; }
+    public required string Category { set; get; }
+    public bool IsCancelled { set; get; }
+    public required string City { set; get; }
+    public required string Venue { set; get; }
+    public required string Latitude { set; get; }
+    public required string Longitude { set; get; }
+
+}
