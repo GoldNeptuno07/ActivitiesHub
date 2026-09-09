34dbe51b6a7d46e068ee2f379a79aa61451fb302
Angel Cruz <angelcruz@Angels-MacBook-Air.local>
2026-09-08 20:45:02 -0600

    feat(parcial01) - add db initializer

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -16,6 +16,21 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) { }
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedDataAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 app.MapControllers();
 
 app.Run();
